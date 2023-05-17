31d3d58332874f234d101222e9ebcee6f3fbb194
Dorilian BRAULT <dorilianb@icloud.com>
2023-05-17 19:12:19 +0200

    Structure

--- cube/src/app/app.module.ts
+++ cube/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from '../pages/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { MatMenuModule } from '@angular/material/menu';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +16,7 @@ import { RegisterComponent } from './pages/register.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent,
-    RegisterComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
