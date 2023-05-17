732c282bd1ce360386bf61d401de807c3b5b97d9
Dorilian BRAULT <dorilianb@icloud.com>
2023-05-17 18:45:18 +0200

    #

--- cube/src/app/home/home.component.ts
+++ cube/src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-index',
+  selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
