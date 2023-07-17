79e578c4c5febad45612d38da3b3bc266ccd6f06
Dorilian <71562603+Pixuuu@users.noreply.github.com>
2023-07-17 10:10:02 +0200

    Structuration (Guards)

---
+++ cube/src/app/guards/admin.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> | boolean {
+    return this.authService.isAdmin().pipe(
+      map(isAdmin => {
+        if (isAdmin) {
+          return true;
+        } else {
+          this.router.navigate(['']); // Redirection à home si l'utilisateur n'est pas Admin
+          return false;
+        }
+      })
+    );
+  }
+}
---
+++ cube/src/app/guards/connected.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectedGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(): Observable<boolean> | boolean {
+    return this.authService.isConnected().pipe(
+      map(isConnected => {
+        if (isConnected) {
+          return true;
+        } else {
+          this.router.navigate(['']); // Redirection à home si l'utilisateur n'est pas Admin
+          return false;
+        }
+      })
+    );
+  }
+  
+}
